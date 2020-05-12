552738c1cb141e866cc593c0c89f6ef5c8b583eb
Maximiliano Molina <hpmaxi@gmail.com>
2020-05-12 08:42:52 -0300

    Error with not enough data points

--- app/src/components/market/common/history-chart/chart.tsx
+++ app/src/components/market/common/history-chart/chart.tsx
@@ -5,6 +5,7 @@ import { bigNumberify } from 'ethers/utils'
 import { AreaChart, XAxis, YAxis, Area } from 'recharts'
 import { ThemeContext } from 'styled-components'
 import moment from 'moment'
+
 type Props = {
   holdingSeries: Maybe<HistoricData>
 }
@@ -29,34 +30,36 @@ export const HistoryChart: React.FC<Props> = ({ holdingSeries }) => {
 
   const themeContext = useContext(ThemeContext)
 
-  return data ? (
-    <AreaChart
-      width={500}
-      height={300}
-      data={data}
-      stackOffset="expand"
-      margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-    >
-      <XAxis dataKey="date" />
-      <YAxis tickFormatter={toPercent} />
-      {data.map(({ date }, index) => {
-        const color = themeContext.outcomes.colors[index]
+  return holdingSeries && data ? (
+    holdingSeries.length <= 1 ? (
+      <div>There's not enough historical data for this market</div>
+    ) : (
+      <AreaChart
+        width={500}
+        height={300}
+        data={data}
+        stackOffset="expand"
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+      >
+        <XAxis dataKey="date" />
+        <YAxis tickFormatter={toPercent} />
+        {data.map(({ date }, index) => {
+          const color = themeContext.outcomes.colors[index]
 
-        return (
-          color && (
-            <Area
-              type="monotone"
-              dataKey={index}
-              key={`${index}-${date}`}
-              stackId="1"
-              stroke="#8884d8"
-              fill={color.darker}
-            />
+          return (
+            color && (
+              <Area
+                type="monotone"
+                dataKey={index}
+                key={`${index}-${date}`}
+                stackId="1"
+                stroke="#8884d8"
+                fill={color.darker}
+              />
+            )
           )
-        )
-      })}
-    </AreaChart>
-  ) : (
-    <div>no-data</div>
-  )
+        })}
+      </AreaChart>
+    )
+  ) : null
 }
